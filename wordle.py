fad0b6f73ce75f72ce961f52ca592358d478f54a
Lucas Curran <92184180+LucasCur@users.noreply.github.com>
2023-01-13 14:40:57 +0000

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -4,63 +4,65 @@ format = """            ┏━━━━━━━━┓
             ┃ WORDLE ┃
             ┗━━━━━━━━┛
 
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓   ┏━━━┓
-┃ ✘ = Incorrect Letter & Placement ┃   ┃ # ┃
-┃ ! = Correct Letter & Wrong Place ┃   ┣━━━┫
-┃ ✔ = Correct Letter & Placement   ┃   ┃ # ┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛   ┣━━━┫  ┏━━━━━━━┓
-                                       ┃ # ┃  ┃ TRIES ┃
- ┏━━━┓  ┏━━━┓  ┏━━━┓  ┏━━━┓  ┏━━━┓     ┣━━━┫  ┗━━━━━━━┛
- ┃ ? ┃  ┃ ? ┃  ┃ ? ┃  ┃ ? ┃  ┃ ? ┃     ┃ # ┃
- ┣━━━┫  ┣━━━┫  ┣━━━┫  ┣━━━┫  ┣━━━┫     ┣━━━┫
- ┃ 1 ┃  ┃ 2 ┃  ┃ 3 ┃  ┃ 4 ┃  ┃ 5 ┃     ┃ # ┃
- ┗━━━┛  ┗━━━┛  ┗━━━┛  ┗━━━┛  ┗━━━┛     ┗━━━┛
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓   
+┃ ✘ = Incorrect Letter & Placement ┃   
+┃ ! = Correct Letter & Wrong Place ┃   ┏━━━┳━━━┓ 
+┃ ✔ = Correct Letter & Placement   ┃   ┃ # ┃ # ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛   ┣━━━╋━━━┫  ┏━━━━━━━┓
+                                       ┃ # ┃ # ┃  ┃ TRIES ┃
+ ┏━━━┓  ┏━━━┓  ┏━━━┓  ┏━━━┓  ┏━━━┓     ┣━━━╋━━━┫  ┗━━━━━━━┛
+ ┃ ? ┃  ┃ ? ┃  ┃ ? ┃  ┃ ? ┃  ┃ ? ┃     ┃ # ┃ # ┃
+ ┣━━━┫  ┣━━━┫  ┣━━━┫  ┣━━━┫  ┣━━━┫     ┗━━━┻━━━┛
+ ┃ 1 ┃  ┃ 2 ┃  ┃ 3 ┃  ┃ 4 ┃  ┃ 5 ┃
+ ┗━━━┛  ┗━━━┛  ┗━━━┛  ┗━━━┛  ┗━━━┛     
 """
 
-t = format
+# Sets the first display to have crosses in place of numbers so that display format is same across all rounds.
+fTF = format
 for i in range(1,6):
-  t = t.replace(str(i),"✘")
-print(t)
+  fTF = fTF.replace(str(i),"✘")
+print(fTF)
 
-def calcdisp(y, z, b):
-  x = format
-  if y == z:
-    x = x.replace("#","✘",b-1)
-    x = x.replace("#","✔",1)
+# Function that runs through the display format and calculates each part of the user's input that lines up with the correct guess etc. while replacing the placeholder values in the display format to be displayed.
+def calcdisp(userGuess, correctGuess, b):
+  display = format
+  if userGuess == correctGuess:
+    display = display.replace("#","✘",b-1)
+    display = display.replace("#","✔",1)
   else:
-    x = x.replace("#","✘",b)
-  
-  for a in range(0,5):
-    if y[a] == z[a]:
-      x = x.replace(str(a+1),"✔")
+    display = display.replace("#","✘",b)
+  for iter in range(0,5):
+    if userGuess[iter] == correctGuess[iter]:
+      display = display.replace(str(iter+1),"✔")
     else:
-      if y[a] in z:
-        x = x.replace(str(a+1),"!")
+      if userGuess[iter] in correctGuess:
+        display = display.replace(str(iter+1),"!")
       else:
-        x = x.replace(str(a+1),"✘")
-  for a in range(0,5):
-    x = x.replace("?",y[a],1)
-  print(x)
+        display = display.replace(str(iter+1),"✘")
+  for iter in range(0,5):
+    display = display.replace("?",userGuess[iter],1)
+  print(display)
 
-ar = json.load(open("words.json", 'r'))
-target = r.choice(ar)
-#print(target) 
+words = json.load(open("data.json", 'r'))
+toGuess = r.choice(words)
+#print(toGuess) 
 #the above is just used for testing so that you can see the random word chosen
 
 guessed = False
 count = 0
 while guessed == False:
   user_guess = input("┃ Guess: ")
-  os.system("clear")
-  calcdisp(user_guess,target,count+1)
-  count += 1
-  if count >= 5 :
-    print("""┏━━━━━━━━━━┓
-┃ YOU LOST ┃
-┗━━━━━━━━━━┛""")
-    break
-  if user_guess == target:
-    print("""┏━━━━━━━━━┓
-┃ YOU WON ┃
-┗━━━━━━━━━┛""")
-    break
+  if user_guess in words:
+    os.system("clear")
+    calcdisp(user_guess,toGuess,count+1)
+    count += 1
+    if count >= 6 :
+      print("""       ┏━━━━━━━━━━┓ ┏━━━━━━━┓
+       ┃ YOU LOST ┃ ┃ """ + str(toGuess) + """ ┃
+       ┗━━━━━━━━━━┛ ┗━━━━━━━┛""")
+      break
+    if user_guess == toGuess:
+      print("""            ┏━━━━━━━━━┓
+            ┃ YOU WON ┃
+            ┗━━━━━━━━━┛""")
+      break
